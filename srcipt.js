2e1e38aa3ab9fa8f7f1e7ebbc0dd60b8ad914289
vitoriamonteiroalunainfor-max <vitoriamonteiroalunainfor@gmail.com>
2026-04-24 08:51:53 -0300

    Update srcipt.js

--- srcipt.js
+++ srcipt.js
@@ -1,1 +1,13 @@
-
+function gerarFrase() {
+    
+    let frases = [
+        "Você consegue, não desista!",
+        "Vai dar certo!",
+        "Seja Grato!",
+        "Confie no processo!",
+        "Um passo de cada vez!"
+    ];
+    
+    let numero = Math.floor(Math.random()* frases.length);
+    document.getElementById("Frase").innerHTML = frases[numero];
+}
